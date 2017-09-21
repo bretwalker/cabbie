80cd543c067ebd068e9d43101ba59915d407097d
Bret Walker <bw@bretw.com>
2017-09-21 17:11:15 -0400

    First commit

---
+++ cabbie/drivers/admin.py
@@ -0,0 +1,16 @@
+import pdb
+from django.contrib import admin
+
+from .models import Description, Driver
+
+class DriverAdmin(admin.ModelAdmin):
+    readonly_fields = ('driver_descriptions',)
+    list_display = ['last_name', 'first_name', 'email', ]
+
+    def driver_descriptions(self, obj):
+        descs = obj.descriptions.all()
+        #pdb.set_trace()
+        return ", ".join([d.title for d in descs]) if descs else "None"
+
+admin.site.register(Description)
+admin.site.register(Driver, DriverAdmin)
